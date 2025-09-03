63439744303ab62dc900c9d6ff4cb0e2c6154444
unknown <happer64bit@gmail.com>
2025-09-03 21:09:14 +0630

    Update apps/todo-service/src/todo/todo.module.ts

---
+++ apps/todo-service/src/todo/todo.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TodoController } from './todo.controller';
+import { TodoService } from './todo.service';
+import { todoProviders } from './todo.provider';
+import { DatabaseModule } from 'src/database/database.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [DatabaseModule, ClientsModule.register({
+    clients: [
+      {
+        name: "RABBIT_MQ",
+        transport: Transport.RMQ,
+        options: {
+          urls: [process.env.QUEUE_URL as string],
+          queue: "todo_queue",
+          queueOptions: {
+            durable: false
+          }
+        }
+      }
+    ]
+  })],
+  controllers: [TodoController],
+  providers: [TodoService, ...todoProviders]
+})
+export class TodoModule { }
