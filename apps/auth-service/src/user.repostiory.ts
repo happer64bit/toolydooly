43e43354bf3a08d51d60de2d6b849dc5a6884618
unknown <happer64bit@gmail.com>
2025-09-03 21:09:08 +0630

    Update apps/auth-service/src/user.repostiory.ts

---
+++ apps/auth-service/src/user.repostiory.ts
@@ -0,0 +1,12 @@
+import knex from 'knex';
+import config from './knexfile';
+const db = knex(config);
+
+export const findUserByIdentifier = (identifier: string) =>
+    db("users").where("email", identifier).where("is_active", true).orWhere("username", identifier).first();
+
+export const findUserById = (uid: string) =>
+    db("users").where("uid", uid).where("is_active", true).first();
+
+export const createUserInDb = (user: { username: string; email: string; password: string }) =>
+    db("users").insert(user).where("is_active", true).returning("*");
