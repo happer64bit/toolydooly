c3a69de41a7103787c96764d81d357da59307934
unknown <happer64bit@gmail.com>
2025-09-03 21:09:07 +0630

    Update apps/auth-service/src/server.ts

---
+++ apps/auth-service/src/server.ts
@@ -0,0 +1,27 @@
+import { urlencoded } from "body-parser";
+import express, { type Express } from "express";
+import morgan from "morgan";
+import { createUser, loginUser, refresh, verify } from "./auth.controller";
+import AuthMiddleware from "./auth.middleware";
+import cookieParser from "cookie-parser";
+import cors from 'cors';
+
+export const createServer = (): Express => {
+  const app = express();
+  app
+    .disable("x-powered-by")
+    .use(morgan("dev"))
+    .use(cors({
+      origin: "http://localhost:5173",
+      credentials: true
+    }))
+    .use(urlencoded({ extended: true }))
+    .use(express.json())
+    .use(cookieParser())
+    .post("/create-user", createUser)
+    .post("/login", loginUser)
+    .get("/session", AuthMiddleware, verify)
+    .get("/refresh", refresh)
+
+  return app;
+};
