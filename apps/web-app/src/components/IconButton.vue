fa5bf3b452c5b4a17d1f9300388c84d18d4b6f88
unknown <happer64bit@gmail.com>
2025-09-03 21:09:20 +0630

    Update apps/web-app/src/components/IconButton.vue

---
+++ apps/web-app/src/components/IconButton.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { defineProps } from 'vue'
+
+type ButtonProps = {
+  type?: 'button' | 'submit' | 'reset'
+  onClick?: (event: MouseEvent) => void
+}
+
+const props = defineProps<ButtonProps>()
+</script>
+
+<template>
+  <button
+    :type="props.type || 'button'"
+    @click="props.onClick"
+    class="bg-black text-white min-w-10 min-h-10 rounded-full p-0 flex items-center justify-center cursor-pointer hover:bg-black/80"
+  >
+    <slot />
+  </button>
+</template>
