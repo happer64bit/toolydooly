447fccb1c97cb2283037916467c8f814aaf911c3
unknown <happer64bit@gmail.com>
2025-09-03 21:09:20 +0630

    Update apps/web-app/src/components/BottomChatContainer.vue

---
+++ apps/web-app/src/components/BottomChatContainer.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import IconButton from './IconButton.vue'
+import { PlusIcon } from '@radix-icons/vue'
+import { ref } from 'vue'
+
+const prompt = ref("")
+
+const emit = defineEmits<{
+    (e: "submit", value: string): void
+}>()
+
+const onSubmit = (e: Event) => {
+    e.preventDefault()
+    if (!prompt.value.trim()) return
+    emit("submit", prompt.value)
+    prompt.value = ""
+}
+</script>
+
+<template>
+    <form class="container rounded-full border border-neutral-300 shadow-sm flex items-center gap-2" @submit="onSubmit">
+        <input v-model="prompt" placeholder="Add a new task..." class="w-full px-6 py-3 outline-0" />
+        <IconButton type="submit">
+            <PlusIcon color="white" class="w-5 h-5" />
+        </IconButton>
+    </form>
+</template>
