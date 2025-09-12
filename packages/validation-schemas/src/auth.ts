2515e558e36e28c10f67061cd1dd47890d5939bb
happer64bit <happer64bit@gmail.com>
2025-09-12 19:14:26 +0630

    make validation stronger

--- packages/validation-schemas/src/auth.ts
+++ packages/validation-schemas/src/auth.ts
@@ -1,14 +1,16 @@
 import { z } from "zod";
 
 export const createUserSchema = z.object({
-  email: z.email().transform((val) => val.toLowerCase()),
+  email: z.email().max(255).transform((val) => val.toLowerCase()),
   username: z.string()
+    .min(3)
+    .max(255)
     .transform(val => val.toLowerCase())
     .refine(val => /^[a-z0-9]+$/.test(val), { message: "Username can only contain lowercase letters and numbers, no spaces or symbols" }),
-  password: z.string().min(8)
+  password: z.string({ error: "Password is required" }).min(8).max(255)
 });
 
 export const loginUserSchema = z.object({
-  usernameOrEmail: z.string().transform((value) => value.toLowerCase()),
-  password: z.string().min(8),
+  usernameOrEmail: z.string().max(255).transform((value) => value.toLowerCase()),
+  password: z.string().min(8).max(255),
 });
