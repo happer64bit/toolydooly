78e44346c0b5c21751cdfc4f9dde311d6d0e7030
happer64bit <happer64bit@gmail.com>
2025-09-17 14:15:26 +0630

    dark mode support

--- apps/web-app/src/components/IconButton.vue
+++ apps/web-app/src/components/IconButton.vue
@@ -10,7 +10,7 @@ const props = defineProps<ButtonProps>()
 
 <template>
   <button :type="props.type || 'button'" @click="props.onClick" :disabled="disabled ?? false"
-    class="bg-black dark:bg-white dark:hover:bg-white/70 dark:text-black text-white min-w-10 min-h-10 rounded-full p-0 flex items-center justify-center cursor-pointer hover:bg-black/80">
+    class="bg-black dark:bg-white disabled:opacity-70 dark:hover:bg-white/70 dark:text-black text-white min-w-10 min-h-10 rounded-full p-0 flex items-center justify-center disabled:cursor-not-allowed not-disabled:cursor-pointer hover:bg-black/80">
     <slot />
   </button>
 </template>
